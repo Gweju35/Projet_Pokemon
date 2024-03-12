d7d2a286b69c814fd9d06f567cb5c144b9dc165a
jperriguey <julien.perriguey@etudiant.univ-rennes1.fr>
2024-03-12 16:24:11 +0100

    test

--- JSON/pokemon.js
+++ JSON/pokemon.js
@@ -1,54 +1,54 @@
 class Pokemon {
     constructor(base_attack, base_defense, base_stamina, form, pokemon_id, pokemon_name) {
-        this.base_attack = base_attack;
-        this.base_defense = base_defense;
-        this.base_stamina = base_stamina;
-        this.form = form;
-        this.pokemon_id = pokemon_id;
-        this.pokemon_name = pokemon_name;
+        this._base_attack = base_attack;
+        this._base_defense = base_defense;
+        this._base_stamina = base_stamina;
+        this._form = form;
+        this._pokemon_id = pokemon_id;
+        this._pokemon_name = pokemon_name;
     }
 
     static all_pokemons = {};
 
     get base_attack() {
-        return this.base_attack;
-    }   
+        return this._base_attack;
+    }
 
     get base_defense() {
-        return this.base_defense;
+        return this._base_defense;
     }
 
     get base_stamina() {
-        return this.base_stamina;
+        return this._base_stamina;
     }
 
     get form() {
-        return this.form;
+        return this._form;
     }
 
     get pokemon_id() {
-        return this.pokemon_id;
+        return this._pokemon_id;
     }
 
     get pokemon_name() {
-        return this.pokemon_name;
+        return this._pokemon_name;
     }
 
     toString() {
-        return `Pokemon: ${this.pokemon_name}, ID: ${this.pokemon_id}, Form: ${this.form}, 
-        Base Attack: ${this.base_attack}, Base Defense: ${this.base_defense}, Base Stamina: ${this.base_stamina}`;
+        return `Pokemon: ${this._pokemon_name}, ID: ${this._pokemon_id}, Form: ${this._form}, 
+        Base Attack: ${this._base_attack}, Base Defense: ${this._base_defense}, Base Stamina: ${this._base_stamina}`;
     }
 
-    import_pokemon(data) {
-        
-        data.forEach(pokemonData => {
-            const { base_attack, base_defense, base_stamina, form, pokemon_id, pokemon_name } = pokemonData;
-            const pokemon = new Pokemon(base_attack, base_defense, base_stamina, form, pokemon_id, pokemon_name);
-            all_pokemons[pokemon.pokemon_id] = pokemon;
+    static import_pokemon(source) {
+        source.forEach(pokemonData => {
+            let pokemon = new Pokemon(pokemonData.base_attack, pokemonData.base_defense, pokemonData.base_stamina, pokemonData.form, pokemonData.pokemon_id, pokemonData.pokemon_name);
+            if (!Pokemon.all_pokemons[pokemon.pokemon_id]) {
+                Pokemon.all_pokemons[pokemon.pokemon_id] = [];
+            }
+            Pokemon.all_pokemons[pokemon.pokemon_id].push(pokemon);
         });
-    
-        return all_pokemons;
+        return Pokemon.all_pokemons;
     }
 }
 
-
+console.log(Pokemon.import_pokemon(pokemons.pokemon));
