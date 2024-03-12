77010bf3fff2838831f6d6ea029bdd3cd63b59c2
jperriguey <julien.perriguey@etudiant.univ-rennes1.fr>
2024-03-12 17:17:45 +0100

    type + efficacité

--- JSON/type.js
+++ JSON/type.js
@@ -1,10 +1,3 @@
-/* À partir des fichiers pokemon_type.json et type_effectiveness.json, proposez une
-classe Type. Ce type permettra donc de connaître l’efficacité d’un type d’attaque contre
-un type de défenseur. N’oubliez pas la méthode toString(). De même que pour les
-Pokémons, on ne gardera que les types des formes dont la valeur est Normal. Une
-variable de classes nommée all_types contiendra l’ensemble des types de Pokémons
-et sera indexé par le nom du type. */
-
 class Type {
     constructor(name, effectiveness) {
         this._name = name;
@@ -25,16 +18,24 @@ class Type {
         return `Type: ${this._name}`;
     }
 
-    static import_types(pokemon_types, type_effectiveness) {
-        pokemon_types.forEach(pokemonTypeData => {
+    static import_types(source, type_effectiveness) {
+        source.forEach(pokemonTypeData => {
             if (pokemonTypeData.form === "Normal") {
                 pokemonTypeData.type.forEach(typeName => {
                     if (!Type.all_types[typeName]) {
-                        Type.all_types[typeName] = new Type(typeName, type_effectiveness[typeName]);
+                        let effectiveness = type_effectiveness[typeName] || {};
+                        Type.all_types[typeName] = new Type(typeName, effectiveness);
                     }
                 });
             }
         });
         return Type.all_types;
     }
+
+    static getEffectiveness(attackingType, defendingType) {
+            return Type.all_types[attackingType].effectiveness[defendingType];
+
+    }
 }
+
+
